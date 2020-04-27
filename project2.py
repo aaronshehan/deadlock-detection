4217efce6c979e76b517f1e2a54d527745580b0f
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-04-27 15:48:27 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,5 +1,9 @@
 from sys import argv
 import re
+try: 
+    import queue
+except ImportError:
+    import Queue as queue
 
 def readFile(filename):
     lines = []
@@ -32,12 +36,40 @@ def parseLines(lines):
     return (num_processes, num_resources, units, matrix)
 
 
+def bfs(matrix, start):
+    q = queue.Queue()
+    q.put(start)
+    visited = {start: True}
+    
+    while not q.empty():
+        current_node = q.get()
+        
+        for i, j in enumerate(matrix):
+            if matrix[current_node][i] != 0 and i not in visited:
+                q.put(i)
+                visited[i] = True
+
+    return visited
+
+
 def main(filename):
     parsedInfo = parseLines(readFile(filename))
     num_processes = parsedInfo[0]
     num_resources = parsedInfo[1]
     units = parsedInfo[2]
     matrix = parsedInfo[3]
+
+    count = 0
+    for i in range(len(matrix)):
+        visited = bfs(matrix, i)
+
+        if len(visited) == len(matrix):
+            count += 1
+
+    if count == len(matrix):
+        print('Deadlocks exists.')
+    else:
+        print('Deadlock does not exist.')
 
 
 if __name__ == '__main__':
